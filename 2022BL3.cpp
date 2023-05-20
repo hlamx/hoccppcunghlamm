ab16263bca7eae2bb14e8eb19dcbc84daa0691c1
Hlamx <88639120+hlamx@users.noreply.github.com>
2023-05-20 15:38:35 +0700

    Add files via upload

--- 2022BL3.cpp
+++ 2022BL3.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long n,a[1000001],i,s;
+long long a[1000001],i,n,d,tam;
 int main()
 {
     ifstream fin("vannghe.inp");
@@ -8,11 +8,14 @@ int main()
     fin>>n;
     for(i=1;i<=n;i++) fin>>a[i];
     sort(a+1,a+n+1);
-      for(i=1;i<=n;i++) cout<<a[i]<<" ";
-    for(i=1;i<=n;i++){
-    if(a[i]==a[i+1]) {s+=2;}
+    a[0]=0;a[n+1]=0;
+    for(i=1;i<=n;i++) {
+        if(a[i]==a[i-1]||a[i]==a[i+1]) {
+        d+=1;
+        }
     }
-    cout<<endl<<s;
+    fout<<d;
     fin.close();
     fout.close();
+    return 0;
 }
