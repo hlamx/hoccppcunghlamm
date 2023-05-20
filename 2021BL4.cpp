115f4cf269ff1c41d99bfbe0261dfa68166aa9b5
Hlamx <88639120+hlamx@users.noreply.github.com>
2023-05-20 12:49:36 +0700

    Update 2021BL4.cpp
    Khai báo và khởi tạo biến:
    hihi được khai báo là một hằng số có giá trị 1e6 + 1 (tức là 1000001).
    a và b là hai mảng có kích thước hihi (1000001) chứa các số nguyên dùng để lưu trữ dữ liệu.
    Các biến n, i, j, k, kq, m, s, minn, maxx và dem được khai báo để thực hiện các phép toán và tính toán trong chương trình.
    Mở file input và file output:
    Chương trình mở file input có tên "dieuche.inp" để đọc dữ liệu đầu vào.
    Chương trình mở file output có tên "dieuche.out" để ghi kết quả sau khi tính toán.
    Đọc dữ liệu từ file input:
    Sử dụng lệnh fin >> n >> k để đọc hai giá trị n và k từ file input.
    Sử dụng vòng lặp for để đọc n giá trị vào mảng a từ file input.
    Sắp xếp mảng a theo thứ tự tăng dần:
    Sử dụng hàm sort(a + 1, a + n + 1) để sắp xếp mảng a từ vị trí 1 đến vị trí n theo thứ tự tăng dần.
    Vòng lặp chính:
    Vòng lặp while sẽ tiếp tục chạy cho đến khi không thỏa mãn điều kiện (i == n && j > m) hoặc (j == m && i > n).
    Biến s được khởi tạo bằng 0 và được sử dụng để tính tổng các phần tử được chọn.
    Biến minn được khởi tạo với giá trị rất lớn (1e15) và biến maxx chưa được khởi tạo.
    Biến dem được khởi tạo bằng 0 và đếm số lượng phần tử được chọn.
    Trong vòng lặp con, chương trình sẽ chọn phần tử nhỏ nhất từ hai mảng a và b hoặc từ mảng a nếu chỉ còn phần tử trong a, sau đó cộng giá trị phần tử này vào s.
    Trong quá trình chọn phần tử, chương trình cập nhật maxx và minn để tìm giá trị lớn nhất và nhỏ nhất trong các phần tử được chọn.
    Sau khi chọn đủ k phần tử, chương trình tính kq bằng hiệu giá trị lớn nhất và nhỏ nhất (maxx - minn), sau đó tăng biến m lên 1 và lưu tổng s vào mảng b.
    Quá trình lặp sẽ tiếp tục cho đến khi không thỏa mãn điều kiện vòng lặp chính.
    Ghi kết quả vào file output và đóng file input và file output:
    Chương trình ghi giá trị của biến kq vào file output bằng lệnh fout << kq.
    Sau đó, chương trình đóng file input và file output bằng lệnh fin.close() và fout.close().

--- 2021BL4.cpp
+++ 2021BL4.cpp
@@ -1,27 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long kq,t,n,a[1000001],i,k,s,m=1;
-int main()
-{
-    ifstream fin("dieuche.inp");
-    ofstream fout("dieuche.out");
-    fin>>n>>k;
-    for(i=1;i<=n;i++) fin>>a[i];
-    sort(a+m,a+n+1);
-    while(m<n){
-        sort(a+m,a+n+1);
-        s=0;
-        if(n-m+1<k) k = n-m+1;
-        for(i=m;i<=m+k-1;i++){
-            s+=a[i];
-        }
-        kq=kq+a[m+k-1]-a[m];
-        n++;
-        a[n] = s;
-        m=m+k;
+long long hihi = 1e6+1, a[hihi], b[hihi], n, i, j, k, kq, m, s, minn = 1e15, maxx, dem;
+
+int main() {
+    ifstream fin("dieuche.inp"); // Mở file input
+    ofstream fout("dieuche.out"); // Mở file output
+    fin >> n >> k; // Đọc giá trị n và k từ file input
+
+    for (i = 1; i <= n; i++) {
+        fin >> a[i]; // Đọc dữ liệu vào mảng a từ file input
     }
-    fout<<kq;
-    fin.close();
-    fout.close();
+
+    sort(a + 1, a + n + 1); // Sắp xếp mảng a theo thứ tự tăng dần
+
+    while (!(i == n && j > m) || !(j == m && i > n)) { // Vòng lặp chạy cho đến khi thỏa mãn điều kiện đủ
+        s = 0;
+        minn = 1e15;
+        maxx; // Tham số chưa được khởi tạo
+        dem;
+
+        while (dem < k && (i <= n || j <= m)) {
+            long long x; // Biến x chưa được khai báo
+            if (i > n) {
+                x = b[j];
+                j++;
+            }
+            else if (j > m) {
+                x = b[i];
+                i++;
+            }
+            else if (a[i] < b[j]) {
+                x = a[i];
+                i++;
+            }
+            else {
+                x = b[j];
+                j++;
+            }
+
+            s += x;
+            maxx = min(maxx, x);
+            minn = min(minn, x);
+            dem++;
+        }
+
+        kq += maxx - minn;
+        m++;
+        b[m] = s;
+    }
+
+    fout << kq; // Ghi kết quả vào file output
+    fin.close(); // Đóng file input
+    fout.close(); // Đóng file output
     return 0;
 }
