d92d77b22ba7dcef59132c2a7b4474d92684badc
Hlamx <88639120+hlamx@users.noreply.github.com>
2023-05-23 12:41:54 +0700

    Update banhchung.cpp

--- banhchung.cpp
+++ banhchung.cpp
@@ -1,8 +1,8 @@
 //bai 3 tin hoc tre 
-//tu 12h10 -> 12h16
+//tu 12h10 -> 12h41
 #include <bits/stdc++.h>
 using namespace std;
-long long v[1000001],n,i,m,tam,s;
+long long v[1000001],n,i,m,tam,s,a;
 int main()
 {
     ifstream fin("banhchung.inp");
@@ -10,7 +10,10 @@ int main()
     fin>>n>>m;
     for(i=1;i<=m;i++) fin>>v[i];
     sort(v+1,v+m+1);
-    for(i=1;i<=m;i++)  if(s<m) s+=v[i];
+    for(i=1;i<=m;i++){
+        if(s<=n) s+=v[i];
+        if(s>=n) s-=v[i];
+    }
     fout<<s;
     fin.close();
     fout.close();
