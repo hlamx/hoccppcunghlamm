1c2314ea3d89f9f29f8bac74c9e385842fda18cf
Hlamx <88639120+hlamx@users.noreply.github.com>
2023-05-23 12:44:09 +0700

    Update banhchung.cpp

--- banhchung.cpp
+++ banhchung.cpp
@@ -7,11 +7,19 @@ int main()
 {
     ifstream fin("banhchung.inp");
     ofstream fout("banhchung.out");
+    // nhập n,m lần lượt là
+    //n = thể tích nồi bánh
+    //m là số bánh
     fin>>n>>m;
+    //chạy dòng for để nhập v[i]
+    //v[i] là thể tích bánh
     for(i=1;i<=m;i++) fin>>v[i];
+    //sort v[i] từ bé đến lớn
     sort(v+1,v+m+1);
     for(i=1;i<=m;i++){
+    // xét trường hợp tổng còn nhỏ hơn n thì ta cộng cho v[i]
         if(s<=n) s+=v[i];
+        // xét trường hợp tổng lớn hơn n thì ta trừ cho v[i]
         if(s>=n) s-=v[i];
     }
     fout<<s;
